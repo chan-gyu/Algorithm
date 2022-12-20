b159ba9bc2d9cd7d09807781f28c491c07a65b88
admin407 <admin407@admin407-pc>
2022-12-20 17:05:55 +0900

    commit

--- Algorithm/src/programmers/level1/Main.java
+++ Algorithm/src/programmers/level1/Main.java
@@ -1,12 +1,13 @@
 package programmers.level1;
 
-import programmers.level2.Solution2_03;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Solution2_03 s = new Solution2_03();
-		System.out.println(s.solution("0111010"));
+		//Solution2_03 s = new Solution2_03();
+		Solution60 s = new Solution60();
+		int n=5;
+		int[] nums = {1,2,7,6,4};
+		System.out.println(s.solution("AaZz",25));
 	}
 	
 }
